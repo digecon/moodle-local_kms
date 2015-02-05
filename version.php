0df2e6a93370625ad41e785d622ee2449785b89d
Sergey Sachik <serega3000@gmail.com>
2015-02-05 23:32:17 +0300

    version updated

--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
-$plugin->version  = 2014122902;   // The (date) version of this plugin
+$plugin->version  = 2014020500;   // The (date) version of this plugin
 $plugin->requires = 2014000000;   // Requires this Moodle version
